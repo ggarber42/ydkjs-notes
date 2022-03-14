e9d35cc5a22519bf7fffc6cbe98ea10434ecceef
ggarber42 <ggarber@protonmail.com>
2022-03-14 17:14:44 -0300

    newbie compairson

--- appendixB/compairson.js
+++ appendixB/compairson.js
@@ -1,3 +1,18 @@
+const dayStart = '07:30'
+const dayEnd = '17:45'
+
+/**
+ * It converts string do an array of numbers
+ * 
+ * @param {string} hours 
+ * @returns {Number[]} converted elment
+ */
+function converteToNumber(hours) {
+     return hours
+          .split(':')
+          .map(element => Number(element))
+} 
+
 /**
  * It tells if meet can be schedule.
  * 
@@ -6,9 +21,19 @@
  * @returns {boolean}
  */
 function scheduleMeeting(startTime, durationMinutes) {
-     const [startHours, startMinutes] = startTime.split(':')
-     console.log(startHours, startMinutes)
-     return false
+     const [hours, minutes] = converteToNumber(startTime)
+     const [startHours, startMinues] = converteToNumber(dayStart)
+     const [endHours, endMinutes] = converteToNumber(dayEnd)
+     if(hours < startHours){
+          return false
+     } else if (hours === startHours && minutes < startMinues){
+          return false
+     } else if (hours === endHours && ( minutes + durationMinutes) > endMinutes){
+          return false
+     } else if (hours > endHours){
+          return false
+     }
+     return true
 }
 
 module.exports = scheduleMeeting
--- appendixB/tests/compairson.test.js
+++ appendixB/tests/compairson.test.js
@@ -2,11 +2,6 @@ const compairson = require('../compairson')
 
 describe('Compairson', () => {
 
-     beforeEach(() => {
-          const dayStart = '07:30'
-          const dayEnd = '17:45'
-     })
-
      it('always returns a boolean', () => {
           expect(typeof compairson('7:00', 15)).toBe('boolean')
      })
@@ -18,5 +13,19 @@ describe('Compairson', () => {
           expect(compairson('7:30', 15)).toBe(true)
      })
 
-     it.todo('returns false if the meeting is at end day')
+     it('returns true if the meeting is during working hours', () => {
+          expect(compairson('11:30', 60)).toBe(true)
+     })
+
+     it('returns true if the meeting finishes at the end of work hours', () => {
+          expect(compairson('17:00', 45)).toBe(true)
+     })
+
+     it('returns false if the meeting finishes after the end of work hours', () => {
+          expect(compairson('17:30', 30)).toBe(false)
+     })
+
+     it('returns false if the meetingstarts after the end of work hours', () => {
+          expect(compairson('18:00', 30)).toBe(false)
+     })
 })
