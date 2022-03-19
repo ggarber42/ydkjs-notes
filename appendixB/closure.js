65a1bcb668ca0771cad7b6e919da6119f5fe6a98
ggarber42 <ggarber@protonmail.com>
2022-03-19 20:16:35 -0300

    closure done

--- appendixB/closure.js
+++ appendixB/closure.js
@@ -4,21 +4,50 @@
  * @param {Number} end 
  * @returns {Number[]} range of numbers
  */
-function range(start,end) {
-     const rangeArray = []
-     if(start === end){
-          rangeArray.push(start)
-     } else if (start < end){
-          let newStart = start
-          while(newStart <= end){
-               rangeArray.push(newStart)
-               newStart += 1
-          }
-     }
-     return rangeArray
- }
+function range(start, end) {
 
- /*
+/* iee não funciona porque perde os valores de start end */     
+/*     return (function (start, end) {
+          const rangeArray = []
+          if (start === end) {
+               rangeArray.push(start)
+          } else if (start < end) {
+               let newStart = start
+               while (newStart <= end) {
+                    rangeArray.push(newStart)
+                    newStart += 1
+               }
+          }
+          return rangeArray
+     })() */
+     if(end === undefined){
+          /* 
+          precisa salvar o start na closure antes pra depois chamar
+          outra função com o end
+          */
+          return function getClosure(end){
+               return getRange(start, end)
+          }
+     } else {
+          return getRange(start, end)
+     }
+
+     function getRange(start, end){
+          const rangeArray = []
+          if (start === end) {
+               rangeArray.push(start)
+          } else if (start < end) {
+               let newStart = start
+               while (newStart <= end) {
+                    rangeArray.push(newStart)
+                    newStart += 1
+               }
+          }
+          return rangeArray  
+     }
+}
+
+/*
 
 range(3,3);    // [3]
 range(3,8);    // [3,4,5,6,7,8]
@@ -34,5 +63,7 @@ start3(0);     // []
 start4(6);     // [4,5,6]
 
 */
+
+range(3,3)
 
 module.exports = range
--- appendixB/tests/closure.test.js
+++ appendixB/tests/closure.test.js
@@ -14,4 +14,16 @@ describe('Closure', () =>{
           const start3 = range(3)
           expect(start3(3)).toStrictEqual([3])
      })
+     it('return a single array using closure with end of 8',() =>{
+          const start3 = range(3)
+          expect(start3(8)).toStrictEqual([3,4,5,6,7,8])
+     })
+     it('return a single array using closure with end of 0',() =>{
+          const start3 = range(3)
+          expect(start3(0)).toStrictEqual([])
+     })
+     it('return a single array using closure 4 with end of 6',() =>{
+          const start4 = range(4)
+          expect(start4(6)).toStrictEqual([4,5,6])
+     })
 })
